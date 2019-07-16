8e6c59cc6f380ea63de10f153a6a4a076f26d5cc
max-ri <max_r_@web.de>
2019-07-16 14:06:05 +0200

    coordinates added to demo guide

--- Guides/Demo_Coldridge_Valley_1-6.lua
+++ Guides/Demo_Coldridge_Valley_1-6.lua
@@ -1,9 +1,10 @@
-Guidelime.registerGuide([[
+Guidelime.registerGuide(
+[[
 [N 1 - 6 Coldridge Valley]
 [GA Dwarf,Gnome]
 [D A short guide for the dwarf and gnome starting zone. Go Gnomes!]
 At the NPC in front of you get [G29.93,71.2Dun Morogh][QA179 Dwarven Outfitters]
-Kill wolves south of you for [QC179 Dwarven Outfitters]
+Kill wolves south of you for [G28.27,74.57][QC179 Dwarven Outfitters]
 Return to the NPC, turn in [G29.93,71.2][QT179 Dwarven Outfitters], and accept [QA3109 Encrypted Rune][A Dwarf,Rogue] and [QA233 Coldridge Valley Mail Delivery]
 Return to the NPC, turn in [G29.93,71.2][QT179 Dwarven Outfitters], and accept [QA3113 Encrypted Memorandum][A Gnome,Rogue] and [QA233 Coldridge Valley Mail Delivery]
 Return to the NPC, turn in [G29.93,71.2][QT179 Dwarven Outfitters], and accept [QA3106 Simple Rune][A Dwarf,Warrior] and [QA233 Coldridge Valley Mail Delivery]
@@ -17,34 +18,34 @@ Left of you accept [G29.71,71.25][QA170 A New Threat]
 Kill troggs along your way for [QC170 A New Threat][O]
 At the camp to the west turn in [G22.6,71.43][QT233 Coldridge Valley Mail Delivery] and accept [QA234 Coldridge Valley Mail Delivery] and [QA183 The Boar Hunter]
 Kill boars in the surrounding area and return to the camp where you turn in [G22.6,71.43][QT183 The Boar Hunter]
-Go to the next camp SE along the path, turn in [G25.08,75.71][QT234 Coldridge Valley Mail Delivery], and accept [QA182 The Troll Cave]
-Kill trolls in the area for [QC182 The Troll Cave][O] until you are level [XP4]
-As soon as you are level [XP4] go back and accept [G24.98,75.96][QA3364 Scalding Mornbrew Delivery]
+Go to the next camp SE along the path, turn in [G25.08,75.7][QT234 Coldridge Valley Mail Delivery], and accept [QA182 The Troll Cave]
+Kill trolls in the area for [G26.13,79.54][QC182 The Troll Cave] until you are level [XP4]
+As soon as you are level [XP4] go back and accept [G24.98,75.95][QA3364 Scalding Mornbrew Delivery]
 [H] Hearth back and enter Anvilmar. Accept [G28.55,67.64][QA3361 A Refugee's Quandary]
 Further north turn in [G28.77,66.37][QT3364 Scalding Mornbrew Delivery] and accept [QA3365 Bring Back the Mug]
 Get new skills at your [T] trainer and turn in [G28.37,67.51][QT3109 Encrypted Rune][A Dwarf,Rogue]
 Get new skills at your [T] trainer and turn in [G28.37,67.51][QT3113 Encrypted Memorandum][A Gnome,Rogue]
-Get new skills at your [T] trainer and turn in [G28.83,67.24][QT3106 Simple Rune][A Dwarf,Warrior]
-Get new skills at your [T] trainer and turn in [G28.83,67.24][QT3112 Simple Memorandum][A Gnome,Warrior]
+Get new skills at your [T] trainer and turn in [G28.83,67.23][QT3106 Simple Rune][A Dwarf,Warrior]
+Get new skills at your [T] trainer and turn in [G28.83,67.23][QT3112 Simple Memorandum][A Gnome,Warrior]
 Get new skills at your [T] trainer and turn in [G28.83,68.33][QT3107 Consecrated Rune][A Dwarf,Paladin]
 Get new skills at your [T] trainer and turn in [G29.18,67.45][QT3108 Etched Rune][A Dwarf,Hunter]
 Get new skills at your [T] trainer and turn in [G28.6,66.39][QT3110 Hallowed Rune][A Dwarf,Priest]
 Get new skills at your [T] trainer and turn in [G28.71,66.37][QT3114 Glyphic Memorandum][A Gnome,Mage]
-Get new skills at your [T] trainer, turn in [G28.65,66.14][QT3115 Tainted Memorandum], and accept [QA1599 Beginnings][A Warlock]
-Exit Anvilmar and finish [QC170 A New Threat]
+Get new skills at your [T] trainer, turn in [G28.65,66.14][QT3115 Tainted Memorandum][A Gnome,Warlock], and accept [QA1599 Beginnings][A Warlock]
+Exit Anvilmar and finish [G26.77,74.82][QC170 A New Threat]
 Turn in [G29.71,71.25][QT170 A New Threat] at the starting point
-Turn in [G24.98,75.96][QT3365 Bring Back the Mug] at the camp at the end of the road
-Loot the box for [QC3361,1 A Refugee's Quandary] at the troll camp at [G21,76]
-Loot the chest for [QC3361,2 A Refugee's Quandary] at the troll camp at [G23,80]
-Loot the bucket for [QC3361,3 A Refugee's Quandary] at the troll camp at [G26,79]
-At the camps or in the cave finish [QC182 The Troll Cave]
-Go back to the camp, turn in [G25.08,75.71][QT182 The Troll Cave], and accept [QA218 The Stolen Journal]
-In the troll cave keep left until you find and kill Grik'nir the Cold for [QC218 The Stolen Journal][A Warrior,Mage,Hunter,Rogue,Priest,Paladin]
-Kill novices in the southern half of the cave until you finish [QC1599 Beginnings][A Warlock]
-At the end of the cave kill Grik'nir the Cold for [QC218 The Stolen Journal][A Warlock]
-Turn in [G25.08,75.71][QT218 The Stolen Journal] at the dwarf camp and accept [QA282 Senir's Observations]
+Turn in [G24.98,75.95][QT3365 Bring Back the Mug] at the camp at the end of the road
+Loot the box for [G20.88,76.06][QC3361,1 A Refugee's Quandary] at the troll camp
+Loot the chest for [G22.78,80][QC3361,2 A Refugee's Quandary] at the troll camp
+Loot the bucket for [G26.33,79.28][QC3361,3 A Refugee's Quandary] at the troll camp
+At the camps or in the cave finish [G26.13,79.54][QC182 The Troll Cave]
+Go back to the camp, turn in [G25.08,75.7][QT182 The Troll Cave], and accept [QA218 The Stolen Journal]
+In the troll cave keep left until you find and kill Grik'nir the Cold for [G30.48,80.15][QC218 The Stolen Journal][AWarrior,Mage,Hunter,Rogue,Priest,Paladin]
+Kill novices in the southern half of the cave until you finish [G29.66,81.37][QC1599 Beginnings][A Warlock]
+At the end of the cave kill Grik'nir the Cold for [G30.48,80.15][QC218 The Stolen Journal][AWarlock]
+Turn in [G25.08,75.7][QT218 The Stolen Journal] at the dwarf camp and accept [QA282 Senir's Observations]
 [H] Hearth or die to get back and turn in [G28.55,67.64][QT3361 A Refugee's Quandary] in Anvilmar
-Exit Anvilmar and follow the path east and turn in [G33.48,71.84][QT282 Senir's Observations] and accept [QA420 Senir's Observations]
-To your right accept [G33.85,72.24][QA2160 Supplies to Tannok]
+Exit Anvilmar and follow the path east and turn in [G33.47,71.84][QT282 Senir's Observations] and accept [QA420 Senir's Observations]
+To your right accept [G33.85,72.23][QA2160 Supplies to Tannok]
 Leave the area through Coldridge Pass
 ]], "Demo")
