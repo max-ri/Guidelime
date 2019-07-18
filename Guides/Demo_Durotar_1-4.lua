2e2128236e2422c09617a9fddcbc9b23daf5cee9
max-ri <max_r_@web.de>
2019-07-18 17:45:30 +0200

    added Durotar demo

---
+++ Guides/Demo_Durotar_1-4.lua
@@ -0,0 +1,46 @@
+Guidelime.registerGuide(
+[[
+[N1-4Durotar]
+[GA Orc,Troll]
+[D A guide created by *Sevenleaves*\\https://www.twitch.tv/7Leaves]
+
+Accept [QA4641 Your Place In The World][G43.29,68.53Durotar].
+
+Rush your way to the tree & accept [QA790 Sarkoth][G40.6,62.58].
+
+If other players are around, invite them to make sure you get the tag. Sarkoth has group loot.[OC]
+Go and kill [QC790 Sarkoth][G40.5,66.83] and loot him.
+Go turn in the quest [QT790 Sarkoth][G40.6,62.58] at the tree and accept [QA804 Sarkoth part 2][G40.6,62.58]
+Run back to Valley of Trials to deliver the quest[QT4641 Your Place In The World][G42.06,68.33] & [QT804 Sarkoth Part 2][G42.06,68.33]. Accept [QA788 Cutting Teeth][G42.06,68.33]
+
+Take the quest [QA792 Vile Familiars][G42.85,69.15].
+Run up to the cave north while grinding mobs and complete [QC792][G44.41,55.45]. Inivite players to get this done as fast as possible.
+Make sure you are level [XP3] before going back to turn in quests. 
+Walk back to town and [V]sell all the items you can't use.
+Turn in [QT792 Vile Familiars][G42.85,69.15] & pick up [QA794 Burning Blade Medallion][G42.85,69.15].
+Take the quest [QA5441 Lazy Peons][G44.63,68.65].
+
+Go and start [QC5441 Lazy Peons][G44.27,62.21] by running up the right side of Valley of Trials while grinding as many mobs as possible.
+Once you reach the cave, kill all the mobs you see until you get to [QC794 Yarrog Baneshadow and kill him for the Medallion][G42.71,52.95]
+
+Run out of the cave and finish [QC5441 Lazy Peons][G44.27,62.21]. Keep grinding mobs whenever possible heading back to turn in your quests.
+
+[V] Sell the items you can't use
+Turn in [QT788 Cutting Teeth][G42.06,68.33] if you managed to do it[O]
+Deliver [QT794 Burning Blade Medallion][G42.85,69.15] & accept [QA805 Report to Sen'jin Village][G42.85,69.15]
+
+Deliver [QT5441 Lazy Peons][G44.63,68.65] and accept the follow up [QA6394 Thazz'ril's Pick][G44.63,68.65].
+
+Head back to the cave to grab [QC6394 Thazz'ril's Pick][G43.73,53.79].
+
+Run to the back of the cave to die/drown your way to Razor Hill.[OC]
+
+Take the spirit res and accept these 2 quests [QA784 Vanquish the Betrayers][G51.95,43.5] & [QA791 Carry Your Weight][G49.88,40.39]. 
+
+[H] Hearth back to Valley of Trials
+
+Turn in[QT6394][G44.63,68.65] & head towards Sen'jin Village.
+
+[NX 4-12 Durotar]
+
+]], "Demo")
