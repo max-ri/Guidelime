fcbfffedb6724a6937b498d07c18ced0f5d984d8
max-ri <51713394+max-ri@users.noreply.github.com>
2019-11-29 11:30:50 +0100

    dont interfere with quest log when neither show quest level nor show quest id is enabled
    also show D/R/+ in quest log for dungeon/raid/elite quests

--- Guidelime/Guidelime_QuestLog.lua
+++ Guidelime/Guidelime_QuestLog.lua
@@ -3,6 +3,8 @@ local addonName, addon = ...
 local L = addon.L
 
 function addon.updateQuestLog()
+	if not GuidelimeData.showQuestLevels and not GuidelimeData.showQuestIds then return end
+
 	local numEntries, numQuests = GetNumQuestLogEntries();
 	
 	if (numEntries == 0) then return end
@@ -15,19 +17,29 @@ function addon.updateQuestLog()
 			title, level, _, isHeader, _, _, _, id = GetQuestLogTitle(questIndex)
 			
 			if (not isHeader) then
+				local newTitle = title
 				if GuidelimeData.showQuestLevels then
-					title = format("  [%d] ", level) .. title
+					local qtype = ""
+					if addon.questsDB[id].type == "Dungeon" then 
+						qtype = "D" 
+					elseif addon.questsDB[id].type == "Raid" then 
+						qtype = "R" 
+					elseif addon.questsDB[id].type == "Elite" then 
+						qtype = "+" 
+					end
+					newTitle = format("  [%d%s] ", level, qtype) .. title
 				end
 				if GuidelimeData.showQuestIds then
-					title = title .. format(" (#%d)", id)
+					newTitle = title .. format(" (#%d)", id)
 				end
 				--title = title .. " |T" .. addon.icons.MAP .. ":12|t"
-				questLogTitle = _G["QuestLogTitle"..i]
-				questCheck = _G["QuestLogTitle"..i.."Check"]
-				questLogTitle:SetText(title)
-				QuestLogDummyText:SetText(title)
-				questCheck:SetPoint("LEFT", questLogTitle, "LEFT", QuestLogDummyText:GetWidth()+24, 0);
-				
+				if newTitle ~= title then
+					questLogTitle = _G["QuestLogTitle"..i]
+					questCheck = _G["QuestLogTitle"..i.."Check"]
+					questLogTitle:SetText(newTitle)
+					QuestLogDummyText:SetText(newTitle)
+					questCheck:SetPoint("LEFT", questLogTitle, "LEFT", QuestLogDummyText:GetWidth()+24, 0);
+				end
 				--questLogTitle.tooltip = "tooltip"
 				--questLogTitle:SetScript("OnEnter", function(self) if self.tooltip ~= nil and self.tooltip ~= "" then GameTooltip:SetOwner(self, "ANCHOR_RIGHT",0,-32);  GameTooltip:SetText(self.tooltip); GameTooltip:Show(); addon.showingTooltip = true end end)
 				--questLogTitle:SetScript("OnLeave", function(self) if self.tooltip ~= nil and self.tooltip ~= "" and addon.showingTooltip then GameTooltip:Hide(); addon.showingTooltip = false end end)
